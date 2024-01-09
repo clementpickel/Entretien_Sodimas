8b2e09c0df3a371fcf46a11df1832e895122ea3e
Clément Pickel <clement.pickel@epitech.eu>
2024-01-09 22:45:22 +0100

    [+] docker marche

--- date-formater-api/public/index.php
+++ date-formater-api/public/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$request->server->set('SCRIPT_NAME', '/index.php');  // Add this line
+
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
